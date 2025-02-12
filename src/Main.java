9ebb0cb75e62befbacc9ea2148b9e87940b1ea88
MuffinHeiler240 <jonathangreive@gmail.com>
2025-02-12 12:14:05 +0100

    Added basic GUI structure

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,54 @@
+import sportverein.Mannschaft;
+import sportverein.Spieler;
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class Main {
     private ArrayList<sportverein.Spieler> spieler;
-    private ArrayList<sportverein.Spieler> mannschaften;
+    private ArrayList<sportverein.Mannschaft> clubs;
     private sportverein.Turnier turnier;
-    /**
-     * @param args the command line arguments
-     */
+    private ArrayList<String> clubNames;
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+                
+        SwingUtilities.invokeLater(() -> {
+
+
+
+            JFrame frame = new JFrame("Basic GUI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 300);
+
+            JMenuBar menuBar = new JMenuBar();
+            JMenu clubMenu = new JMenu("Mannschaft");
+            JMenu playerMenu = new JMenu("Spieler");
+            JMenu turnamentMenu = new JMenu("Turnier");
+
+            JMenuItem createClub = new JMenuItem("Erstellen");
+            JMenuItem editCLub = new JMenuItem("Bearbeiten");
+            JMenuItem deleteClub = new JMenuItem("Löschen");
+
+            clubMenu.add(createClub);
+            clubMenu.add(editCLub);
+            clubMenu.addSeparator();
+            clubMenu.add(deleteClub);
+
+            menuBar.add(clubMenu);
+            menuBar.add(playerMenu);
+            menuBar.add(turnamentMenu);
+
+            frame.setJMenuBar(menuBar);
+
+            JPanel panel = new JPanel();
+            panel.setLayout(new FlowLayout());
+
+            frame.add(panel);
+            frame.setVisible(true);
+        });
     }
 
     private sportverein.Mannschaft mannschaftErstellen()
@@ -20,5 +60,28 @@ public class Main {
     private void mannschaftBearbeiten(sportverein.Mannschaft team)
     {
 
+    }
+    private void deleteClub(int clubId){
+        for(int i = 0; i < clubs.size(); i++){
+            if(clubs.get(i).ClubId == clubId){
+                clubs.remove(i);
+                break;
+            }
+        }
+    }
+    private Spieler createPlayer(){
+        Spieler test = null;
+        return test;
+    }
+    private void deletePlayer(int spielerId){
+        for(int i = 0; i < spieler.size(); i++){
+            if(spieler.get(i).getPlayerId() == spielerId){
+                spieler.remove(i);
+                break;
+            }
+        }
+    }
+    private void deleteTurnier(){
+        turnier = null;
     }
 }
